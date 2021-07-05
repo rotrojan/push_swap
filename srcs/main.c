03b357ba30eac3f743a9e7f8bb83b6c3ffaee99a
Romain TROJANI-BIGOT <rotrojan@e1r3p10.42.fr>
2021-07-05 16:51:22 +0200

    Removed useless braces.

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rotrojan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 16:29:24 by rotrojan          #+#    #+#             */
-/*   Updated: 2021/07/04 21:02:45 by bigo             ###   ########.fr       */
+/*   Updated: 2021/07/05 16:50:39 by rotrojan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,15 @@ int	main(int ac, char **av)
 
 	if (ac < 2)
 		return (0);
+	error = parse_args(av);
+	if (error != NO_ERROR)
 	{
-		error = parse_args(av);
-		if (error != NO_ERROR)
-		{
-			free_stack(A);
-			ft_putendl_fd("Error", STDERR_FILENO);
-			return (EXIT_FAILURE);
-		}
-		order_stack(A);
-		sort_stack(A);
 		free_stack(A);
+		ft_putendl_fd("Error", STDERR_FILENO);
+		return (EXIT_FAILURE);
 	}
+	order_stack(A);
+	sort_stack(A);
+	free_stack(A);
 	return (EXIT_SUCCESS);
 }
